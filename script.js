322cbb293aae1ae544ebc17fdda22707a67e3e31
okumu87 <okumu793@gmail.com>
2026-02-21 14:50:33 +0100

    working on card draw

--- script.js
+++ script.js
@@ -10,16 +10,23 @@ function handleClick() {
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+document.getElementById("new-deck").addEventListener("click",handleClick)
 
-document.getElementById("draw-card").addEventListener("click", () => {
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=1`)
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            document.getElementById("cards").innerHTML = 
-                `<img src="${data.cards[0].image}" class="card">
-                <img src="${data.cards[1].image}" class="card">`
+            console.log(data.cards)
+           
+             document.getElementById("cards").children[0].innerHTML = 
+             `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            document.getElementById("cards").children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `
         })
 })
+
+
 
